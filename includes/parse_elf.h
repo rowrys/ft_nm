52d5505dbafc0811d701966aa0c98366848b2c90
lasere77 <colin.mewen@gmail.com>
2026-07-27 12:29:32 +0200

    feat(elf hdr): implement elf info struct.

--- includes/parse_elf.h
+++ includes/parse_elf.h
@@ -5,8 +5,17 @@
 # include "file.h"
 
 # include <stdbool.h>
+#include <stdint.h>
 # include <string.h>
 # include <elf.h>
+
+typedef struct s_elf_info {
+	uint16_t	nb_phdr;
+	uint16_t	nb_shdr;
+	uint16_t	size_phdr;
+	uint16_t	size_shdr;
+	bool		is_x64;
+} t_elf_info;
 
 static inline bool
 is_elf(t_file* file) {
--- srcs/parse_elf.c
+++ srcs/parse_elf.c
@@ -12,46 +12,48 @@
 #include <sys/mman.h>
 #include <unistd.h>
 
-// static void
-// parse_shdr(t_file* file, bool is_x64, uint16_t nb_phdrf) {
-// 	uint64_t	shoff;
-// 	Elf64_Shdr*	shrd;
+#include <endian.h>
 
-// 	memcpy(&shoff, ptr_add(file->buffer, OFFSET_EHDR(is_x64, e_shoff)), 8 >> !is_x64);
-// 	for (size_t i = 0; i < nb_phdrf; ++i) {
-// 		shrd = ptr_add(file->buffer, OFFSET_EHDR(is_x64, e_shstrndx));
-// 		int strtab_offset = shrd->sh_offset;
-// 		printf("%s\n", (uchar*)file->buffer + strtab_offset);
-// 	}
-// }
-
-static void
-parse_elf(t_ctx* ctx, t_file* file, char* file_path) {
+static bool get_elf_info(t_elf_info* elf_info, t_file* file, t_ctx* ctx, char* file_path) {
 	static uint16_t const	et_rel = ET_REL;
 	static uint16_t const	et_dyn = ET_DYN;
-	bool					is_x64;
-	uint16_t				nb_phdr;
-	uint16_t				nb_shdr;
 
 	if (!is_elf(file)) {
 		dprintf(STDERR_FILENO, "nm: %s: file format not recognized\n", file_path);
 		ctx->exit_code = 1;
-		return ;
+		return (1);
 	}
-	is_x64 = (((uchar*)file->buffer)[EI_CLASS] == (uchar)ELFCLASS64);
-	if (((uchar*)file->buffer)[EI_CLASS] != (uchar)ELFCLASS32 && !is_x64) {
+	elf_info->is_x64 = (((uchar*)file->buffer)[EI_CLASS] == (uchar)ELFCLASS64);
+	if (((uchar*)file->buffer)[EI_CLASS] != (uchar)ELFCLASS32 && !elf_info->is_x64) {
 		dprintf(STDERR_FILENO, DEFAULT_ERROR_MSG); /* Invalid class */
 		ctx->exit_code = 1;
-		return ;
+		return (1);
 	}
-	if (!memcmp(&((uchar*)file->buffer)[OFFSET_EHDR(is_x64, e_type)], &et_rel, 2) && !memcmp(&((uchar*)file->buffer)[OFFSET_EHDR(is_x64, e_type)], &et_dyn, 2)) {
+	if (!memcmp(&((uchar*)file->buffer)[OFFSET_EHDR(elf_info->is_x64, e_type)], &et_rel, 2)
+			&& !memcmp(&((uchar*)file->buffer)[OFFSET_EHDR(elf_info->is_x64, e_type)], &et_dyn, 2)) {
 		dprintf(STDERR_FILENO, DEFAULT_ERROR_MSG); /* Invalid file format not (.so .o x86(_64)) */
 		ctx->exit_code = 1;
-		return ;
+		return (1);
 	}
-	memcpy(&nb_phdr, &((uchar*)file->buffer)[OFFSET_EHDR(is_x64, e_phnum)], 2);
-	memcpy(&nb_shdr, &((uchar*)file->buffer)[OFFSET_EHDR(is_x64, e_shnum)], 2);
-	// parse_shdr(file, is_x64, nb_phdr);
+	memcpy(&elf_info->nb_phdr, ((uchar*)file->buffer) + OFFSET_EHDR(elf_info->is_x64, e_phnum), 2);
+	memcpy(&elf_info->nb_shdr, ((uchar*)file->buffer) + OFFSET_EHDR(elf_info->is_x64, e_shnum), 2);
+	memcpy(&elf_info->size_phdr, ((uchar*)file->buffer) + OFFSET_EHDR(elf_info->is_x64, e_phentsize), 2);
+	memcpy(&elf_info->size_shdr, ((uchar*)file->buffer) + OFFSET_EHDR(elf_info->is_x64, e_shentsize), 2);
+	return (0);
+}
+
+static void
+parse_elf(t_ctx* ctx, t_file* file, char* file_path) {
+	t_elf_info	elf_info;
+
+	if (get_elf_info(&elf_info, file, ctx, file_path))
+		return ;
+
+	printf("nb programme hdr{%d}\n", elf_info.nb_phdr);
+	printf("size programme hdr{%d}\n", elf_info.size_phdr);
+	printf("nb section hdr{%d}\n", elf_info.nb_shdr);
+	printf("size section hdr{%d}\n", elf_info.size_shdr);
+	// parse_shdr(file, elf_info.is_x64, elf_info.nb_phdr);
 	printf("%s:\n", file_path);
 	return ;
 }
