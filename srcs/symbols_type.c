bbe7cb4b476702a4edf69aa7c221322e4f12ef38
lasere77 <colin.mewen@gmail.com>
2026-07-31 11:14:10 +0200

    feat(symbols type): implement SHT_NOTE it behave like SHT_PROGBITS

--- srcs/symbols_type.c
+++ srcs/symbols_type.c
@@ -1,21 +1,8 @@
  
 #include "symbols_type.h"
+#include "file.h"
 #include "utils.h"
 #include "parse_elf.h"
-
-static char
-get_type_from_progbits(t_elf_info* elf_info, void* symbol_relation_section) {
-	uint64_t	s_hdr_flags;
-
-	shdrcpy_flags(&s_hdr_flags, symbol_relation_section, elf_info->is_x64);
-	if (s_hdr_flags & SHF_ALLOC && s_hdr_flags & SHF_WRITE) // .data
-		return ('d');
-	else if (s_hdr_flags & SHF_ALLOC && s_hdr_flags & SHF_EXECINSTR) // .text
-		return ('t');
-	else if (s_hdr_flags & SHF_ALLOC)	// .rodata
-		return ('r');
-	return ('?');
-}
 
 // static char*
 // get_name_of_s_hdr(void* s_hdr, t_file* file, t_elf_info* elf_info) {
@@ -29,29 +16,46 @@ get_type_from_progbits(t_elf_info* elf_info, void* symbol_relation_section) {
 // 	return (ptr_add(file->buffer, s_hdr_strtab_offset + s_hdr_name));
 // }
 
+static char
+get_type_from_progbits(t_elf_info* elf_info, void* symbol_relation_section) {
+	uint64_t	s_hdr_flags;
+
+	shdrcpy_flags(&s_hdr_flags, symbol_relation_section, elf_info->is_x64);
+	if (s_hdr_flags & SHF_ALLOC && s_hdr_flags & SHF_WRITE) // .data
+		return ('d');
+	else if (s_hdr_flags & SHF_ALLOC && s_hdr_flags & SHF_EXECINSTR) // .text
+		return ('t');
+	else if (s_hdr_flags == SHF_ALLOC) // .note.ABI-tag
+		return ('r');
+	else if (s_hdr_flags & SHF_ALLOC)	// .rodata
+		return ('r');
+	return ('?');
+}
+
 char
 get_symbos_type(t_elf_info* elf_info, t_file* file, void* sym_hdr) {
 	void*		symbol_relation_section;		/* Elf64_Shdr/Elf32_Shdr */
 	bool		is_maj;
 	uint32_t	s_hdr_type;
+	uint32_t	s_hdr_name;
 	uint16_t	sym_hdr_shndx;
 	uchar		sym_hdr_info;
 
 	symhdrcpy_info(&sym_hdr_info, sym_hdr, elf_info->is_x64);
 	symhdrcpy_shndx(&sym_hdr_shndx, sym_hdr, elf_info->is_x64);
-	is_maj = (ELF32_ST_BIND(sym_hdr_info) == STB_GLOBAL);
+	is_maj = (ELF32_ST_BIND(sym_hdr_info) & STB_GLOBAL);
 	if (sym_hdr_shndx >= elf_info->nb_shdr)
-		return ('u' - (32 * is_maj));
+		return ('*');
 	symbol_relation_section = get_s_hdr_by_index(ptr_add(file->buffer, elf_info->shoff), sym_hdr_shndx, elf_info->is_x64);
 	shdrcpy_type(&s_hdr_type, symbol_relation_section, elf_info->is_x64);
-	// printf(" section name{%s} ", get_name_of_s_hdr(symbol_relation_section, file, elf_info));
+	shdrcpy_name(&s_hdr_name, symbol_relation_section,  elf_info->is_x64);
+	if (ELF32_ST_BIND(sym_hdr_info) & STB_WEAK)
+		return (convert_gloal_symbol('w', is_maj));
 	switch (s_hdr_type) {
-		case SHT_NOBITS:
+		case SHT_NOBITS:	// .bss
 			return (convert_gloal_symbol('b', is_maj));
-		case SHT_PROGBITS:
+		case SHT_PROGBITS: case SHT_NOTE:	// .data .rodata .text .note.ABI-tag
 			return (convert_gloal_symbol(get_type_from_progbits(elf_info, symbol_relation_section), is_maj));
 	}
-	if (ELF32_ST_BIND(sym_hdr_info) == STB_WEAK)
-		return ('w' - (32 * is_maj));
-	return ('?');
+	return (convert_gloal_symbol('u', is_maj));
 }
