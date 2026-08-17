fe187a74d54a3ca6d3062ad8bb5e5ef0c2b329bc
lasere77 <colin.mewen@gmail.com>
2026-08-17 14:14:37 +0200

    fix(nm): suppress no symbols message when exit code is set

--- srcs/symbols_info.c
+++ srcs/symbols_info.c
@@ -96,7 +96,8 @@ sort_symbols_value(t_ctx* ctx) {
 void
 display_symbols_info(t_ctx* ctx, char* current_binary_file_path) {
 	if (!ctx->symbols_info_len) {
-		printf("nm: %s: no symbols\n", current_binary_file_path);
+		if (!ctx->exit_code)
+			printf("nm: %s: no symbols\n", current_binary_file_path);
 		return ;
 	}
 	if (!get_option_stat(ctx->options, OPT_P)) {
